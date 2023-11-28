cff63c87fd7c705b113e37103f2d83ca97de621d
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-11-28 13:15:52 -0300

    Configurando Module Alias

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
             <h2>Sobre mim</h2>
             <p>
                 Influenciado pela área de atuação do meu pai, minha trajetória acadêmica evoluiu para uma 1ª graduação em Engenharia Mecânica,
-                proporcionando-me valiosas experiências através de estágios na área de manutenção industrial.
+                proporcionando-me valiosas experiências através de estágios na área de manutenção industrial.Na 2ª graduação optei por cursar Analise e Desenvolvimento de Sistemas
             </p>
         </div>
 
