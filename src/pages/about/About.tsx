ad107929e78c7c6c9d5685a942443df58284cb27
Paulo Gustavo <pgustavo-siqueira@hotmail.com>
2023-12-02 20:08:40 -0300

    Alterações about

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <div className='about-container'>
             <h2>Sobre mim</h2>
             <p>
-            Graduado em Engenharia Mecânica e atualmente cursando Análise e Desenvolvimento de Sistemas.Desenvolvedor Front-End com experiência em React.js, Redux, JavaScript, TypeScript, Sass, Material UI e consumo de APIs REST. Possuo experiência profissional também com Metodologias Ágeis.
+            Graduado em Engenharia Mecânica e atualmente cursando Análise e Desenvolvimento de Sistemas.Desenvolvedor Front-End com experiência em React.js, Redux, JavaScript, TypeScript, Sass, Material UI e consumo de APIs REST. Possuo experiência profissional também com Metodologias Ágeis.No momento busco por oportunidades de ingressas no mercado como desenvolvedor, como estagiário ou júnior
             </p>
         </div>
 
