fe3be02a56c7483ffcfe84a93f33b1847b953217
Paulo Gustavo Siqueira <99617968+Paulogsiqueira@users.noreply.github.com>
2024-05-16 15:23:41 -0300

    Update About.tsx

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -6,7 +6,7 @@ const About: React.FC = (): ReactElement => {
         <div className='about-container' id="about">
             <h2>Sobre mim</h2>
             <p>
-                Graduado em Engenharia Mecânica e atualmente cursando Análise e Desenvolvimento de Sistemas. Desenvolvedor Front-End com experiência em React.js, Redux, JavaScript, TypeScript, Sass, Material UI e consumo de APIs REST. Possuo experiência profissional também com Metodologias Ágeis. No momento busco por oportunidades de ingressar no mercado como desenvolvedor júnior ou estagiário
+                Graduado em Engenharia Mecânica e atualmente cursando Análise e Desenvolvimento de Sistemas. Desenvolvedor Front-End com experiência em React.js, Redux, JavaScript, TypeScript, Sass, Material UI e consumo de APIs REST. Possuo experiência profissional também com Metodologias Ágeis. No momento busco por oportunidades de ingressar no mercado como desenvolvedor. 
             </p>
 
         </div>
